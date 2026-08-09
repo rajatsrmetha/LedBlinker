e014a842c325e5d495645347f14a095b15ba5635
rajatsrmetha <rajatsrmetha@gmail.com>
2026-08-09 11:58:58 +0530

    LED Blinker Component added

---
+++ BlinkLed/Components/Led/Led.cpp
@@ -0,0 +1,28 @@
+// ======================================================================
+// \title  Led.cpp
+// \author space
+// \brief  cpp file for Led component implementation class
+// ======================================================================
+
+#include "BlinkLed/Components/Led/Led.hpp"
+
+namespace BlinkLed {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+Led ::Led(const char* const compName) : LedComponentBase(compName) {}
+
+Led ::~Led() {}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void Led ::TODO_cmdHandler(FwOpcodeType opCode, U32 cmdSeq) {
+    // TODO
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+}  // namespace BlinkLed
---
+++ BlinkLed/Components/Led/Led.hpp
@@ -0,0 +1,42 @@
+// ======================================================================
+// \title  Led.hpp
+// \author space
+// \brief  hpp file for Led component implementation class
+// ======================================================================
+
+#ifndef BlinkLed_Led_HPP
+#define BlinkLed_Led_HPP
+
+#include "BlinkLed/Components/Led/LedComponentAc.hpp"
+
+namespace BlinkLed {
+
+class Led final : public LedComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Component construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct Led object
+    Led(const char* const compName  //!< The component name
+    );
+
+    //! Destroy Led object
+    ~Led();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Handler implementations for commands
+    // ----------------------------------------------------------------------
+
+    //! Handler implementation for command TODO
+    //!
+    //! TODO
+    void TODO_cmdHandler(FwOpcodeType opCode,  //!< The opcode
+                         U32 cmdSeq            //!< The command sequence number
+                         ) override;
+};
+
+}  // namespace BlinkLed
+
+#endif
